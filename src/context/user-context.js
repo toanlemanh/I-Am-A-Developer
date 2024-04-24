488522ee5a9e847f54c78aae290407c1fbf0edef
longgpgp2 <longnguyenthanh9a@gmail.com>
2024-04-24 18:05:18 +0700

    add usercontext

---
+++ src/context/user-context.js
@@ -0,0 +1,107 @@
+import { createContext, useState } from 'react';
+
+const initialUserState = {
+    id: "",
+    UserDailyLogin: {
+        lastLoginDate: "",
+        currentLoginDate: "",
+        rewards: {
+            money: 0,
+        },
+    },
+    character: {
+        name: "Username",
+        money: 0,
+        age: 0,
+        occupation: "",
+
+        inSchool: false,
+        inUni: false,
+    },
+    status: {
+        health: 100,
+        happiness: 100,
+        look: 100,
+    },
+    ageLogs: {
+
+
+    },
+
+    currentDiseases: [],
+    education: {
+        math: 0,
+        literature: 0,
+        history: 0,
+        science: 0,
+        geography: 0,
+        art: 0,
+        music: 0,
+        physicalEducation: 0,
+        computerScience: 0,
+        english: 0
+    },
+    higherEducation: {
+        computerScience: 0,
+        softwareEngineering: 0,
+        informationTechnology: 0,
+        cybersecurity: 0,
+        dataScience: 0,
+        design: 0,
+        networking: 0,
+        security: 0,
+        statistics: 0,
+        sQL: 0,
+        databaseManagement: 0,
+        projectManagement: 0,
+        communication: 0,
+        businessAnalysis: 0,
+        testing: 0,
+        softwareDevelopment: 0,
+        userExperience: 0,
+        userInterface: 0
+    },
+    assets: {
+
+    },
+    progress: 0,
+};
+
+export const UserContext = createContext(initialUserState);
+
+const UserProvider = ({ children }) => {
+    const [userState, setUserState] = useState(initialUserState);
+
+    const updateUserLogin = (newLoginData) => {
+        setUserState({
+            ...userState,
+            UserDailyLogin: newLoginData,
+        });
+    };
+
+    const updateCharacterStatus = (newStatusData) => {
+        setUserState({
+            ...userState,
+            status: newStatusData,
+        });
+    };
+
+    const updateUserMoney = (newMoney) => {
+        setUserState({
+            ...userState,
+            Info: {
+                ...userState.Info,
+                money: newMoney,
+            },
+        });
+    };
+
+
+    return (
+        <UserContext.Provider value={{ userState, updateUserLogin, updateUserMoney, updateCharacterStatus }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export default UserProvider;
