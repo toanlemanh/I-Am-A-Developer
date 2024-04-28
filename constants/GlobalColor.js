e013578ffc223857ade882c6594837ef46bf1a39
toanlemanh <toanlemanh2003@gmail.com>
2024-04-28 13:46:33 +0700

    create globalcolor

---
+++ constants/GlobalColor.js
@@ -0,0 +1,10 @@
+export const COLOR = {
+    orangeButton: "",
+    headerBackground: "",
+    progressBar: "",
+    greenButton: "",
+    headerItemBackground: "",
+    happinessColor: "",
+    healthColor: "",
+    lookColor: "",
+}
