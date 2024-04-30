627446226059f0fa0274cf838f55a62bd2ef5471
longgpgp2 <longnguyenthanh9a@gmail.com>
2024-04-30 21:40:27 +0700

    finalize

--- api/axios.js
+++ api/axios.js
@@ -3,33 +3,6 @@ import axios from 'axios';
 const BACKEND_URL =
     'https://fb-login-demo-93174-default-rtdb.firebaseio.com/'
 
-export async function storeExpense(data, dataname) {
-    await axios.post(BACKEND_URL + `/${dataname}.json`, data);
-}
-
-export async function fetchUserData(uid) {
-    const response = await axios.get(BACKEND_URL + `/users/${uid}.json`)
-    return response
-}
-
-export async function fetchExpenses() {
-    const response = await axios.get(BACKEND_URL + '/expenses.json')
-
-    const expenses = []
-
-    for (const key in response.data) {
-        const expenseObj = {
-            id: key,
-            amount: response.data[key].amount,
-            date: new Date(response.data[key].date),
-            description: response.data[key].description
-        }
-        expenses.push(expenseObj)
-    }
-
-    return expenses
-}
-
 export function updateExpense(id, expenseData) {
     return axios.put(BACKEND_URL + `/expenses/${id}.json`, expenseData)
 }
@@ -45,13 +18,13 @@ export function getUserId(userId) {
     return axios.get(BACKEND_URL + `/${userId}.json`).then((response) => {
         // Parse response data into an object
         const current = Object.values(response.data)[0];
-     //   console.log("current", current);
+        //   console.log("current", current);
         const key = (Object.keys(response.data)[0]);
-   //     console.log(key + "as");
+        //     console.log(key + "as");
         return { key, current }
     })
 };
-export function putUserData ( userId, currentKey, userData) {
+export function putUserData(userId, currentKey, userData) {
     axios.put(BACKEND_URL + `/${userId}/${currentKey}.json`, userData).then(response => {
         console.log(">>>>> response put: ", response.data)
     });
